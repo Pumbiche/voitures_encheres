0782230171d950f2e4871669dc00b4ad5d527178
Joachim <joachim.bader05@gmail.com>
2025-07-16 09:35:04 +0200

    maj

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,7 @@ from decouple import config
 import os
 import dj_database_url
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
