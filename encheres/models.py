1c1734d893f3a5e24100d946cdd7aa5348623260
Joachim <joachim.bader05@gmail.com>
2025-07-17 14:53:33 +0200

    majj

--- encheres/models.py
+++ encheres/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from decimal import Decimal
+from cloudinary.models import CloudinaryField
 
 class Voiture(models.Model):
     marque = models.CharField(max_length=100)
